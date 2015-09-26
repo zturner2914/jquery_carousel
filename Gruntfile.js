9e470b7f9defc1688743c8bca0cd72d9f1e2fe65
Zachary <zturner29@gmail.com>
2015-09-26 17:28:18 -0500

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,10 @@ module.exports = function(grunt) {
                 cwd: "node_modules/",
                 src: [
                     "jquery/dist/jquery.min.js",
-                    "jquery/dist/jquery.min.map"
+                    "jquery/dist/jquery.min.map",
+                    "bootstrap/dist/css/bootstrap.min.css",
+                    "bootstrap/dist/css/bootstrap.css.map",
+                    "bootstrap/dist/js/bootstrap.min.js"
                 ],
                 "dest": "server/public/vendor/"
             }
