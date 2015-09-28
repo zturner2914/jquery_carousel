8e6691a201ab29bf0b31c794b22406d9674dd8c9
Zachary <zturner29@gmail.com>
2015-09-27 19:42:10 -0500

    maybe

--- client/app.js
+++ client/app.js
@@ -5,6 +5,18 @@ $(function(){
         //console to see if anything works
         console.log(response);
 
+        for (var i = 0; i < response.length; i++) {
+            var active = "";
+            if (i == 0) {
+                active = " active";
+            }
+            var slide = "<div class='item" + active + "'><p>" + response[i].name + "</p><p>" + response[i].description + "</p><p>" + response[i].shoutouts + "</p></div>"
+            $(".carousel-inner").append(slide);
+            $(".carousel-indicators").append("<li data-target='#carousel-example-generic' data-slide-to='" + i + "' class='" + active + "'></li>");
+        }
+
+        $('.carousel').carousel();
+
     });
 
 });
